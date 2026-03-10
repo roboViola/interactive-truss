71ffd71da9c943f7d64db0ce202855236467cd19
roboViola <rachtobey@gmail.com>
2026-03-10 15:53:30 -0400

    Added Structures for Data Transmission

--- message_types.h
+++ message_types.h
@@ -1,4 +1,22 @@
-enum MessageType {
-    MSG_DATA = 0,
-    MSG_RESET = 1
+#ifndef MESSAGE_TYPES_H
+#define MESSAGE_TYPES_H
+
+// Enumerated Datatype for Message Type
+enum MessageType : uint8_t {
+    MSG_DATA,
+    MSG_RESET
 }
+
+// Structure for Sensor Data
+struct sense_msg {
+    MessageType msg_type;
+    float force_data;
+};
+
+// Structure for Reset
+struct reset_msg {
+    MessageType msg_type;
+    bool reset_signal;
+};
+
+#endif
